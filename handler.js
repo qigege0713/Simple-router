43da260e3b2887d32d4dd2436ba9d3042cef0fae
zeq <zeq886@outlook.com>
2021-01-04 15:52:29 +0800

    helo

--- handler.js
+++ handler.js
@@ -1,25 +1,23 @@
 var fs = require("fs");
 var http = require("http");
 
-function home(response){
-    response.writeHead(200,{"Content-Type":"text/html"});
-    fs.createReadStream(__dirname+"/wwwroot/home.html","utf8").pipe(response);
+function home(response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/wwwroot/home.html", "utf8").pipe(response);
 }
 
-function nihao(response){
-    response.writeHead(200,{"Content-Type":"text/html"});
-    fs.createReadStream(__dirname+"/wwwroot/nihao.html","utf8").pipe(response);
+function nihao(response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.createReadStream(__dirname + "/wwwroot/nihao.html", "utf8").pipe(response);
 }
 
+function json(response, params) {
+    response.writeHead(200, { "Content-Type": "application/json" });
 
-
-function json(response,params){
-    response.writeHead(200,{"Content-Type":"application/json"});
-     
     response.end(JSON.stringify(params));
 }
 
-module.exports={
+module.exports = {
     home,
     nihao,
     json
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ function startServer(route, handle) {
     var onRequest = function (request, response) {
         //console.log("request received"+request.url);
         var pathname = url.parse(request.url,).pathname;
-
-
-
         var data = "";
         request.on("error", (err) => {
             console.error(err);
@@ -17,7 +14,7 @@ function startServer(route, handle) {
             data += chunk;
         }).on("end", () => {
             if (request.method === "POST") {
-                route(handle, pathname, response,querystring.parse(data));
+                route(handle, pathname, response, querystring.parse(data));
             } else {
                 var params = url.parse(request.url, true).query;
                 route(handle, pathname, response, params);
@@ -28,6 +25,7 @@ function startServer(route, handle) {
     var server = http.createServer(onRequest);
     server.listen(8888);
     console.log("IP是 Http://127.0.0.1:8888");
+   
 }
 
 
